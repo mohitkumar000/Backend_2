4951bea40f3bccd9963a5b2b21c2abc33a3dc1bc
Mohit kumar <64029983+mohitkumar000@users.noreply.github.com>
2022-12-16 19:14:27 +0530

    first-server express

---
+++ contact_list/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const port = 8000;
+
+const app = express();
+app.set('view engine', 'ejs');//setting the value of 'view engine'
+app.set('views',path.join(__dirname,'views'));
+
+
+app.get('/',function(req,res)
+{
+  //console.log(__dirname);
+  //res.send("<h1>cool it is running</h1>");
+  return res.render('index_home');
+});
+
+
+app.listen(port,function(err) {
+  if(err) {
+    console.log("error in running the server");
+  }
+  console.log("server listening",port);
+});
