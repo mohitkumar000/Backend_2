e09945600778ba8be88e407017f0fb0e338ceb06
Mohit kumar <64029983+mohitkumar000@users.noreply.github.com>
2023-01-20 18:48:00 +0530

    commit 9

---
+++ clouser/callback.js
@@ -0,0 +1,15 @@
+function fun(x,fn)
+{
+    //some logic
+
+    for(let i=0;i<x;i++){
+        console.log(i);
+    }
+    fun();
+}
+
+fun(10,function log()
+{
+    //this is the callback function
+    console.log("custom logger");
+});
