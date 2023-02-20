1a32b44a84b8be5c904b3444bfd5eb193629893a
Mohit kumar <64029983+mohitkumar000@users.noreply.github.com>
2023-02-20 15:54:56 +0530

    commit

---
+++ oops/classintro.js
@@ -0,0 +1,19 @@
+class product{
+    //member function
+
+    constructor(n,p){
+        //constructor function to create a new real life instance called as objets
+        //when we create n object this constructor is first function that gets called
+        this.name = n;
+        this.price =p;
+    }
+
+    displayProduct(){
+
+    }
+
+    buyProduct(){
+
+    }
+
+}
